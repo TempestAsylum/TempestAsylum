c49411685de50a1329806838555e4eb7723054d3
TempestAsylum <TempestAsylum@gmail.com>
2013-02-27 07:47:35 +0200

    Update 8.6

--- TempestAsylum/Models/TournamentModel.vb
+++ TempestAsylum/Models/TournamentModel.vb
@@ -11,13 +11,7 @@ Public Class TournamentModel
 
     Private WebPage As New WebPage
     Private Sub GetDGLInfo()
-        Dim _html As String
-        _html = WebPage.GetHTMLString("http://www.dogamingleague.co.za/stage.php?id=521")
-
-        Dim HtmlDocument As New HtmlDocument
-        HtmlDocument.LoadHtml(_html)
-        Dim var = HtmlDocument.DocumentNode.SelectNodes("//table")
-        Dim _HtmlNodeCollection As HtmlNodeCollection = WebPage.GetNodes(_html, "champion", "/html[1]/body[1]/div[2]/div[1]/table[1]//table[1]")
+        Dim _HtmlNodeCollection As HtmlNodeCollection = WebPage.GetNodes(WebPage.GetHTMLString("http://www.dogamingleague.co.za/stage.php?id=521"), "champion", "/html[1]/body[1]/div[2]/div[1]/table[1]//table[1]")
 
         'Team Rankings - B(11) - C(20) - H(65)
         Dim B = 0
@@ -65,14 +59,15 @@ Public Class TournamentModel
             Week(count) = New List(Of String)
             For Each node As HtmlNode In _HtmlNodeCollection.Item(3 + count).SelectNodes("tr")
                 If node.InnerHtml.Contains("TmA") Then
-                    Dim temp = node.SelectNodes("td")
-                    Week(count).Add(temp.Item(0).InnerText + "  vs  " + temp.Item(2).InnerText)
-                    Dim matchLink = DirectCast(temp.Item(2), HtmlNode).InnerHtml.Split("""")
-                    Dim _matchhtml = WebPage.GetHTMLString(matchLink(1))
-                    Dim matchHtmlDocument As New HtmlDocument
-                    matchHtmlDocument.LoadHtml(_matchhtml)
-                    Dim matchVar = matchHtmlDocument.DocumentNode.SelectNodes("//table")
-                    Match.Add(DirectCast(matchVar.Item(1), HtmlNode).InnerHtml)
+                    'Dim temp = node.SelectNodes("td")
+                    'Week(count).Add(temp.Item(0).InnerText + "  vs  " + temp.Item(2).InnerText)
+                    Week(count).Add(node.InnerHtml)
+                    'Dim matchLink = DirectCast(temp.Item(2), HtmlNode).InnerHtml.Split("""")
+                    'Dim _matchhtml = WebPage.GetHTMLString(matchLink(1))
+                    'Dim matchHtmlDocument As New HtmlDocument
+                    'matchHtmlDocument.LoadHtml(_matchhtml)
+                    'Dim matchVar = matchHtmlDocument.DocumentNode.SelectNodes("//table")
+                    'Match.Add(DirectCast(matchVar.Item(1), HtmlNode).InnerHtml)
                 End If
             Next
             count += 1
--- TempestAsylum/Views/Shared/_Head.vbhtml
+++ TempestAsylum/Views/Shared/_Head.vbhtml
@@ -1,16 +1,16 @@
 ﻿<head>
     <title>Tempest Asylum</title>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <meta name="description" content='Temepst Asylum is a competitive group of people whom play League of Legends.' />
     <meta name="keywords" content="Tempest, Asylum, Tempest Asylum, TmA, tma" />
-    <meta name="distribution" content="web">
-    <meta http-equiv="refresh" content="30">
+    <meta name="distribution" content="web" />
+    <meta http-equiv="refresh" content="30" />
     <meta name="viewport" content="width=device-width" />
     <meta name="apple-mobile-web-app-capable" content="yes" />
     <meta name="apple-mobile-web-app-status-bar-style" content="grey" />
     <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;" />
-    <link href="~/images/favicon.png" rel="shortcut icon"></link>
-    <link href="~/images/favicon.png" rel="bookmark icon"></link>
+    <link href="~/images/favicon.png" rel="shortcut icon" />
+    <link href="~/images/favicon.png" rel="bookmark icon" />
     <meta name="google-site-verification" content="ZOn1V12mVhQ9e93-OVzw3yCAMj8TcWSeGKqCgAr4lAk" />
     @System.Web.Optimization.Styles.Render("~/css/main.css")
     @Html.Partial("~/Views/Shared/body/_Scripts.vbhtml")
